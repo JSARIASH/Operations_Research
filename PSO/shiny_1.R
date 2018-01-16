ac929e50980d3d83ba56cdca46a9356123f35f7c
Sebastian <jsarias@utp.edu.co>
2018-01-16 09:26:33 -0500

    Update ¬¬

--- PSO/shiny_1.R
+++ PSO/shiny_1.R
@@ -1,5 +1,8 @@
 # Algunas funciones para probar la convergencia de los algoritmos se pueden encotrar en wikipedia. 
-# https://en.wikipedia.org/wiki/Test_functions_for_optimization
+#### https://en.wikipedia.org/wiki/Test_functions_for_optimization ####
+
+#### Paquetes requeridos ####
+
 rm(list = ls())
 library(shiny)
 library(rgl)
@@ -8,20 +11,20 @@ library(plot3D)
 #### Estructura Von Neumann   ####
 
 von_neumman <- function(vertices) {
-  vecinos <- matrix(rep(0, 4*vertices), nrow = vertices, ncol = 4)
-  if ( vertices %% 3 == 0 & vertices > 6 ){ # la cantidad de filas va a ser multiplo de tres. 
+  vecinos <- matrix(rep(0, 4 * vertices), nrow = vertices, ncol = 4)
+  if (vertices %% 3 == 0 & vertices > 6){ # la cantidad de filas va a ser multiplo de tres. 
     
     cont_fil <- 1 # fila. 
     
-    for ( i in 1:vertices ){
+    for (i in 1:vertices) {
       
-      if ( cont_fil == 1 ){ # Primera fila 
-          if ( i %% 3 == 0 ){ # última columna
+      if (cont_fil == 1) { # Primera fila 
+          if (i %% 3 == 0) { # última columna
              vecinos[i, 1] <- i - 2
              vecinos[i, 2] <- i - 1
              vecinos[i, 3] <- i * 2
              vecinos[i, 4] <- vertices
-          } else if ( i == (3 * cont_fil - 2) ){ # primera columna
+          } else if (i == (3 * cont_fil - 2)){ # primera columna
              vecinos[i, 1] <- i + 1
              vecinos[i, 2] <- i + 2
              vecinos[i, 3] <- i + 3
@@ -32,13 +35,13 @@ von_neumman <- function(vertices) {
              vecinos[i, 3] <- i + 3
              vecinos[i, 4] <- vertices - 1
           }
-      } else if ( cont_fil == vertices / 3 ){ # última fila
-          if ( i %% 3 == 0 ){ # última columna
+      } else if (cont_fil == vertices / 3) { # última fila
+          if (i %% 3 == 0) { # última columna
              vecinos[i, 1] <- 3 
              vecinos[i, 2] <- i - 3
              vecinos[i, 3] <- i - 2
              vecinos[i, 4] <- i - 1 
-          } else if ( i == (3 * cont_fil - 2) ){ # primera columna
+          } else if (i == (3 * cont_fil - 2)) { # primera columna
              vecinos[i, 1] <- 1   
              vecinos[i, 2] <- i - 3  
              vecinos[i, 3] <- i + 1 
@@ -50,12 +53,12 @@ von_neumman <- function(vertices) {
              vecinos[i, 4] <- i + 1
           }        
       } else { # filas intermedias. 
-          if ( i %% 3 == 0 ){ # última columna
+          if (i %% 3 == 0) { # última columna
              vecinos[i, 1] <- i - 3
              vecinos[i, 2] <- i - 2
              vecinos[i, 3] <- i - 1
              vecinos[i, 4] <- i + 3
-          } else if ( i == (3 * cont_fil - 2) ){ # primera columna
+          } else if (i == (3 * cont_fil - 2)) { # primera columna
              vecinos[i, 1] <- i - 3 
              vecinos[i, 2] <- i + 1 
              vecinos[i, 3] <- i + 2
@@ -63,12 +66,11 @@ von_neumman <- function(vertices) {
           } else {
              vecinos[i, 1] <- i - 3 
              vecinos[i, 2] <- i - 1 
-             vecinos[i, 3] <- i + 1
-             vecinos[i, 4] <- i + 3
+             vecinos[i, 3] <- i + 1     
+             vecinos[i, 4] <- i + 3 
         }
       }
-      
-        if ( i %% 3 == 0 ){
+        if (i %% 3 == 0) {
           cont_fil = cont_fil + 1 
         }   
     }
@@ -76,13 +78,17 @@ von_neumman <- function(vertices) {
   return(vecinos)
 }
 
+#### Mejor solución del vecindario de la partícula i-ésima ####
+
+mejor_veci <- function(vecindad,swarm) {
+  
+}
 
 
-# comentario para subir el master
 
 ##### Variable requeridas para realizar el gráfico de la función a optimizar. #####
 
-#### Función Rastering ####
+##### Función Rastering #####
 # x <- seq(-20.2, 20.2, by = 0.1)
 # y <- x
 # a <- mesh(x, y)
@@ -93,15 +99,23 @@ von_neumman <- function(vertices) {
 # el dominio es -10 <= x,y <= 10. para los valores d1 y d2. 
 # Tener encuenta que cuando se cambia la función objetivo también se cambia la manera en como esta es evaluada. (los z1)
 
-x <- seq(-10,10, by = 0.05)
+x <- seq(-10, 10, by = 0.05)
 y <- x
-a <- mesh(x,y)
-z <- -0.0001*(abs(sin(a$x)*sin(a$y)*exp(abs(100 - sqrt(a$x^2 + a$y^2)/pi ))) + 1)^0.1
+a <- mesh(x, y)
+z <- -0.0001 * (abs(sin(a$x) * sin(a$y) * exp(abs(100 - sqrt(a$x^2 + a$y^2) / pi ))) + 1)^0.1
 
 #### Parámetros y valores iniciales del enjambre. #### 
-n_pariculas <- 10 # cantidad de partículas
+
+n_pariculas <- 9 # cantidad de partículas, Para la estructura de von Neummna deben ser  > 6 y multiplos de 3
 d1 <- runif(n_pariculas, -10, 10) # Coordenadas para la primera dimensión. 
 d2 <- runif(n_pariculas, -10, 10) # Coordenadas para la segunda demensión. 
+c1 <- 1.2
+c2 <- 1.2
+r1 <- diag(runif(2), nrow =  2) # cuadrada respecato a la cantidad de variables. 
+r2 <- diag(runif(2), nrow =  2) # cuadrada respecato a la cantidad de variables.
+
+w_min <- 0.4 # Valores máximo y minímo para controlar la velocidad. 
+w_max <- 0.99 # La velocidad va  decrecer de manera líneal. 
 
 #### z inciales de las funciones####
 #z1 <- 10*2+(d1^2 - 10*cos(2*pi*d1)+d2^2 - 10*cos(2*pi*d2)) # Función objetivo del enjambre. Rastering
@@ -115,18 +129,8 @@ d2A <- vel1
 z1A <- vel1
 swarm  <- cbind(d1, d2, z1, vel1, vel2, d1A, d2A, z1A) # enjambre y función objetivo
 
-# parámetros del algoritmo. 
-
-c1 <- 1.2
-c2 <- 1.2
-r1 <- diag(runif(2), nrow =  2) # cuadrada respecato a la cantidad de variables. 
-r2 <- diag(runif(2), nrow =  2) # cuadrada respecato a la cantidad de variables.
-
-w_min <- 0.4 # Valores máximo y minímo para controlar la velocidad. 
-w_max <- 0.99 # La velocidad va  decrecer de manera líneal. 
-
 #### Mejor solución del enjambre ####
-g_pos <- which(swarm[,3] == min(swarm[,3]))
+g_pos <- which(swarm[, 3] == min(swarm[, 3]))
 G <- swarm[g_pos, 1:3]
 
 
@@ -145,18 +149,18 @@ ui <- fluidPage(theme="simplex.min.css",
                 br(), # se deja un espacio
                 hr(), # se pone una línea horizontal 
                 fluidRow(
-                  column(6,offset = 1,
+                  column(6, offset = 1,
                          sliderInput(inputId = "din",
                                      label = "canti",
-                                     min = 1, max = 500,value = 1,step = 1,
-                                     animate = animationOptions(loop = FALSE,interval = 200)))
+                                     min = 1, max = 500, value = 1, step = 1,
+                                     animate = animationOptions(loop = FALSE, interval = 200)))
                 )
 )
 
 
 server <- function(input, output, session) {
   output$funcion <- renderPlot({
-    surf3D(a$x,a$y,z,theta = 15,phi = 35,bty = "b",shade = 0.1,colvar = z)
+    surf3D(a$x, a$y, z, theta = 15, phi = 35, bty = "b", shade = 0.1, colvar = z)
   })
   
 
@@ -168,7 +172,7 @@ server <- function(input, output, session) {
   #### Mejor solución encontrada como reactivo para poder actualizarlo ####
   G_Opt <- reactiveValues(data = as.data.frame(G))  
   
-  observeEvent(input$din,{
+  observeEvent(input$din, {
     if (input$din == 1) {
       particulas$data <- as.data.frame(swarm)
       G_Opt$data <- as.data.frame(G)
@@ -176,7 +180,7 @@ server <- function(input, output, session) {
   })
   
   #### cambia las partículas cada vez que se actualiza el slide. ####
-  particles <-eventReactive(input$din,{
+  particles <- eventReactive(input$din, {
     # Se actualizan las velocidades y las posiciones. 
     # d1 y d2 representan las mejores personales. 
     G <- as.matrix(G_Opt$data)
@@ -184,7 +188,7 @@ server <- function(input, output, session) {
     
     ##### Se gráfica la posición inicial del las particulas.   ####
     if (input$din == 1){
-      mat <- swarm[,1:2]
+      mat <- swarm[, 1:2]
       particulas$data <- as.data.frame(swarm)
       G_Opt$data <- as.data.frame(G)
       
@@ -192,17 +196,18 @@ server <- function(input, output, session) {
       print(input$din)
       print(swarm)
       print(G)
+
       return(mat)
-    } else if (input$din == 2){
+    } else if (input$din == 2) {
       # Se actualiza la velocidad tentiendo en cuenta el óptimo, Diferencia entre la mejor posición del enjambre 
       # y cada una de las partículas. 
       
       # Las columnas 4 y 5 son las velocidades.   
       # factor de inercia se define por W. 
       W <-  w_max - ((w_max - w_min) / 500) * input$din # 500 máximo número de iteraciones (Definido en el slice). 
-      swarm[,4:5] <- c2*((matrix(rep(G[1:2], n_pariculas), nrow = n_pariculas, byrow = TRUE) - swarm[,1:2]) %*% r2)
-      swarm[,4:5] <- swarm[,4:5] * W
-      swarm[,6:7] <- (swarm[,1:2] + swarm[,4:5])
+      swarm[,4:5] <- c2*((matrix(rep(G[1:2], n_pariculas), nrow = n_pariculas, byrow = TRUE) - swarm[, 1:2]) %*% r2)
+      swarm[,4:5] <- swarm[, 4:5] * W
+      swarm[,6:7] <- (swarm[, 1:2] + swarm[, 4:5])
       
       #### Se evalúa la función objetivo. ####
       
@@ -210,46 +215,49 @@ server <- function(input, output, session) {
       #swarm[,8] <- 10*2 + (swarm[,6]^2 - 10*cos(2*pi*swarm[,6]) + swarm[,7]^2 - 10*cos(2*pi*swarm[,7])) # Rasterin
       
       # Cross in Tray
-      swarm[,8] <- -0.0001 * (abs(sin(swarm[, 6]) * sin(swarm[, 7]) * exp(abs(100 - sqrt(swarm[, 6] ^ 2 + swarm[, 7] ^ 2) / pi ))) + 1) ^ 0.1  # Cross in Tray
+      swarm[, 8] <- -0.0001 * (abs(sin(swarm[, 6]) * sin(swarm[, 7]) * exp(abs(100 - sqrt(swarm[, 6] ^ 2 + swarm[, 7] ^ 2) / pi ))) + 1) ^ 0.1  # Cross in Tray
       
       # se identifica si hay menores
-      menores <- which(swarm[,8] < swarm[,3])
+      menores <- which(swarm[, 8] < swarm[, 3])
       
       # si en la columna 8 todos son mayores la longitud es cero
       # se acualizan las posiciones a una mejor. 
-      if (length(menores) != 0){
-        swarm[menores,1:3] <- swarm[menores,6:8]
+      if (length(menores) != 0) {
+        swarm[menores, 1:3] <- swarm[menores, 6:8]
       } 
-      if (G[3] > min(swarm[,8])){
-        p_update <- which(swarm[,8] == min(swarm[,8]))
-        G <- swarm[p_update,6:8]
+      if (G[3] > min(swarm[, 8])){
+        p_update <- which(swarm[, 8] == min(swarm[, 8]))
+        G <- swarm[p_update, 6:8]
       }
       
       print(input$din)
       print(swarm)
       print(G)
+      
       # estas son las nuevas posiciones
-      mat <- swarm[,6:7]
+      mat <- swarm[, 6:7]
+      
       # se actualiza el enjambre y la el óptimo. 
       particulas$data <- as.data.frame(swarm)
       G_Opt$data <- as.data.frame(G)
+      
       return(mat)
     }else{
-      swarm[,4:5] <-  swarm[, 4:5] + runif(1) * c1 * (swarm[,1:2] - swarm[, 6:7]) %*% r1 +
-        c2*((matrix(rep(G[1:2],n_pariculas),nrow = n_pariculas,byrow = TRUE) - swarm[,6:7]) %*% r2)
+      swarm[, 4:5] <-  swarm[, 4:5] + runif(1) * c1 * (swarm[, 1:2] - swarm[, 6:7]) %*% r1 +
+        c2*((matrix(rep(G[1:2], n_pariculas), nrow = n_pariculas, byrow = TRUE) - swarm[, 6:7]) %*% r2)
       
       # factor de inercia. 
       W <-  w_max - ((w_max - w_min) / 500) * input$din
-      swarm[,4:5] <- swarm[,4:5] * W
+      swarm[, 4:5] <- swarm[, 4:5] * W
       
-      swarm[,6:7] <- (swarm[,4:5] + swarm[,6:7]) %*% (diag(runif(2),nrow =  2)*1.8)
+      swarm[, 6:7] <- (swarm[, 4:5] + swarm[, 6:7]) %*% (diag(runif(2), nrow =  2) * 1.8)
       
       #### Se evalúa la función objetivo. ####
       #Rastering
       #swarm[,8] <- 10*2 + (swarm[,6]^2 - 10*cos(2*pi*swarm[,6]) + swarm[,7]^2 - 10*cos(2*pi*swarm[,7]))
       
       # Cross in Tray
-      swarm[,8] <- -0.0001 * (abs(sin(swarm[, 6]) * sin(swarm[, 7]) * exp(abs(100 - sqrt(swarm[, 6] ^ 2 + swarm[, 7] ^ 2) / pi ))) + 1) ^ 0.1  # Cross in Tray
+      swarm[, 8] <- -0.0001 * (abs(sin(swarm[, 6]) * sin(swarm[, 7]) * exp(abs(100 - sqrt(swarm[, 6] ^ 2 + swarm[, 7] ^ 2) / pi ))) + 1) ^ 0.1  # Cross in Tray
       
       # se identifica si hay menores
       menores <- which(swarm[,8] < swarm[,3])
@@ -261,14 +269,16 @@ server <- function(input, output, session) {
       } 
       if (G[3] > min(swarm[,8])){
         p_update <- which(swarm[,8] == min(swarm[,8]))
-        G <- swarm[p_update,6:8]
+        G <- swarm[p_update, 6:8]
       }
       
       print(input$din)
       print(swarm)
       print(G)
+      
       # estas son las nuevas posiciones
-      mat <- swarm[,6:7]
+      mat <- swarm[, 6:7]
+      
       # se actualiza el enjambre y la el óptimo. 
       particulas$data <- as.data.frame(swarm)
       G_Opt$data <- as.data.frame(G)
