10c6dcbfc67b41b5a23c70c0dd994efd438562eb
Sebastián Arias <sebasariash@gmail.com>
2018-01-16 18:36:46 -0500

    Espacios

--- Gommory/cuadro_optimo.R
+++ Gommory/cuadro_optimo.R
@@ -1,29 +1,29 @@
 cuadro_optimo <- function(m){
   library(MASS)
   dimen <- dim(m) 
-  if (is.null(colnames(m))){
-    colnames(m) <-c("RHS",paste("X",1:(dimen[2]-1),sep=""))
-    rownames(m)<- c("X0",paste("X",(dimen[2]):(dimen[1]+dimen[2]-2),sep=""))
+  if (is.null(colnames(m))) {
+    colnames(m) <- c("RHS", paste("X", 1:(dimen[2]-1), sep = ""))
+    rownames(m) <- c("X0", paste("X", (dimen[2]):(dimen[1] + dimen[2] - 2), sep = ""))
   }
-   while (any(m[1,2:dimen[2]] > 0) == TRUE | any(m[2:dimen[1],1] < 0) == TRUE){
+   while (any(m[1, 2:dimen[2]] > 0) == TRUE | any(m[2:dimen[1], 1] < 0) == TRUE){
       
       #  Si los RHS son positivos (en fila). Se procede a realizar primal.  
-     if (all(m[2:dimen[1],1] >= 0)) {
+     if (all(m[2:dimen[1], 1] >= 0)) {
          # se toma la variable más positiva para entrear a la base. 
          # Se toma el indice de la columna. 
-         entra <- which(m[1,2:dimen[2]]==max(m[1,2:dimen[2]])) + 1
+         entra <- which(m[1, 2:dimen[2]] == max(m[1, 2:dimen[2]])) + 1
          
          # si hay más de un valor se escoge el primero de manera arbitraria. 
-         if (length(entra) > 1){
+         if (length(entra) > 1) {
            entra <- entra[1]
          }
          
          # Se busca la variable a salir de la base. 
          # RHS / columna entrante, Se escoge el minimo mayor a cero. 
-         sale <- m[2:dimen[1],1] / m[2:dimen[1],entra]
+         sale <- m[2:dimen[1], 1] / m[2:dimen[1], entra]
          
          # Se rompe el ciclo si hay solución infactible. 
-         if (all(sale <= 0)){
+         if (all(sale <= 0)) {
            break
          }
          sale <- which(sale == min(sale[sale > 0])) + 1
@@ -34,33 +34,33 @@ cuadro_optimo <- function(m){
          
          # se van a acutaliza los elementos diferentes a la columna y fila pivote.
          
-         for(i in 1:dimen[1]){
-           for(j in 1:dimen[2]){
-             if (i != sale & j != entra){
-                 m[i,j] <- m[i,j] - (m[i,entra]*m[sale,j])/m[sale,entra]
+         for (i in 1:dimen[1]) {
+           for (j in 1:dimen[2]) {
+             if (i != sale & j != entra) {
+                 m[i, j] <- m[i, j] - (m[i, entra] * m[sale, j]) / m[sale, entra]
              }
            }
          }
          # se actualiza la fila y la columna pivote. 
          # Columna pivote se divide entre el elemto pivote y se le cambia el signo. 
-         ele_pivote <- m[sale,entra]
-         m[,entra] <- m[,entra]/ele_pivote * -1
+         ele_pivote <- m[sale, entra]
+         m[, entra] <- m[, entra] / ele_pivote * -1
          
          # La fila pivote se divide entre el elemento pivote. 
-         m[sale,] <- m[sale,]/ele_pivote
+         m[sale, ] <- m[sale, ] / ele_pivote
          
          # Se actualiza el elemento pivote
-         m[sale,entra] <- 1/ele_pivote
+         m[sale, entra] <- 1 / ele_pivote
          
          # se actualiza los nombres de la variable que entra y la que sale. 
          sale_tmp <- rownames(m)[sale]
          rownames(m)[sale] <- colnames(m)[entra]
          colnames(m)[entra] <- sale_tmp
-   }else{ 
+   } else { 
      # de lo contrario se mira el RHS en columna
      # Si hay un RHS negativo a parte de z se reaiza dual simplex. 
      # En el dual simplex sale la mas negativa. 
-     sale <- which(m[2:dimen[1],1] == min(m[2:dimen[1],1])) + 1
+     sale <- which(m[2:dimen[1], 1] == min(m[2:dimen[1], 1])) + 1
      
      # Si hay más de dos se escoge la primera de manera arbitraria. 
      if (length(sale) > 1){
@@ -70,7 +70,7 @@ cuadro_optimo <- function(m){
      # Entra el mínimo cociente mayor o igual a cero entre el z y la fila pivote.
      # No se va a tener en cuenta se tienen en cuenta los valores negativos de la fila pivote. 
      
-     entra <- m[1,2:dimen[2]]/m[sale,2:dimen[2]] 
+     entra <- m[1, 2:dimen[2]] / m[sale, 2:dimen[2]] 
 
      # estos son los cientes mayores o iguales a cero. 
      MayorIgualCero <- which(entra >= 0) 
@@ -85,24 +85,24 @@ cuadro_optimo <- function(m){
        entra <- entra[1]
      }
      
-     for(i in 1:dimen[1]){
-       for(j in 1:dimen[2]){
+     for (i in 1:dimen[1]) {
+       for (j in 1:dimen[2]) {
          if (i != sale & j != entra){
-           m[i,j] <- m[i,j] - (m[i,entra]*m[sale,j])/m[sale,entra]
+           m[i, j] <- m[i, j] - (m[i, entra] * m[sale, j]) / m[sale, entra]
          }
        }
      }
      
      # se actualiza la fila y la columna pivote. 
      # Columna pivote se divide entre el elemto pivote y se le cambia el signo. 
-     ele_pivote <- m[sale,entra]
-     m[,entra] <- m[,entra]/ele_pivote * -1
+     ele_pivote <- m[sale, entra] 
+     m[, entra] <- m[, entra] / ele_pivote * -1
      
      # La fila pivote se divide entre el elemento pivote. 
-     m[sale,] <- m[sale,]/ele_pivote
+     m[sale, ] <- m[sale, ] / ele_pivote
      
      # Se actualiza el elemento pivote
-     m[sale,entra] <- 1/ele_pivote
+     m[sale, entra] <- 1 / ele_pivote
      
      # se actualiza los nombres de la variable que entra y la que sale. 
      sale_tmp <- rownames(m)[sale]
